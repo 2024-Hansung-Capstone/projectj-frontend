9998757f625900bf87b967d35ea1e374065a2f26
cw1662 <chaewon1662@naver.com>
2024-02-05 22:27:56 +0900

    revise Home

--- src/pages/BeforeLogin/Home.js
+++ src/pages/BeforeLogin/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default function Home() {
   return (
     <div className='Home-container'>
-      <h1>홈페이지 로그인 전</h1>
-      <div className='Home-photoList'><h4>카드 사진</h4></div>
+      <div className='Home-photoList'>
+            <img
+              className="card-photo"
+              src="/card3.jpg"
+              alt="로고"
+            />
+      </div>
       <div className='Home-slider1'><h4>요리</h4></div>
       <div className='Home-slider2'><h4>원룸</h4></div>
       <div className='Home-slider3'><h4>자취메이트</h4></div>
