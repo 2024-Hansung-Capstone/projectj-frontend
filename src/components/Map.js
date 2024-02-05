8f945f6428cb8b278fd576c5ee61be441cfd14ac
chimdae <laun@hansung.ac.kr>
2024-02-05 23:56:45 +0900

    edit oneroom page

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,18 +5,16 @@ import 'leaflet/dist/leaflet.css';
 const Map = () => {
   useEffect(() => {
     const mapContainer = document.getElementById('map');
-
-    const map = L.map(mapContainer).setView([37.5665, 126.9780], 13);
+    const map = L.map(mapContainer).setView([37.5828, 127.0106], 16);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors',
     }).addTo(map);
 
-    // useEffect에서 반환하는 함수는 컴포넌트가 언마운트될 때 호출됩니다.
     return () => {
-      map.remove(); // 맵을 제거합니다.
+      map.remove();
     };
-  }, []); // 두 번째 매개변수로 빈 배열을 전달하여 최초 렌더링 시에만 실행되도록 합니다.
+  }, []);
 
   return <div id="map" style={{ height: '100%', width: '100%' }} />;
 };
