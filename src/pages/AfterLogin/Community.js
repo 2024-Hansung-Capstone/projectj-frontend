e73339d238689f56a1d2e9a4fbca51bb1e6b9d5d
cw1662 <chaewon1662@naver.com>
2024-03-07 21:01:14 +0900

    3/7

--- src/pages/AfterLogin/Community.js
+++ src/pages/AfterLogin/Community.js
@@ -1,10 +1,55 @@
-import React from 'react';
-//import './Community.css';
+import React, { useState } from 'react';
+import { FaUtensils } from 'react-icons/fa';
+import { FcHome, FcPaid, FcIdea, FcAdvertising, FcConferenceCall, FcFaq, FcShop } from "react-icons/fc";
+import './Community.css';
+import Community_Item from '../../item/Community_Item';
+
 
 export default function Community() {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItemData, setSelectedItemData] = useState(null);
+
+  const boardListItems = [
+    { title: '원룸 찾기', data: '원룸', icon: <FcHome /> },
+    { title: '인테리어 정보', data: '인테리어 정보', icon: <FcIdea /> },
+    { title: '중고마켓', data: '중고마켓', icon: <FcPaid /> },
+    { title: '맛집추천', data: '맛집추천', icon: <FcShop /> },
+    { title: '요리자랑', data: '요리자랑', icon: <FaUtensils /> },
+    { title: '메이트후기', data: '메이트후기', icon: <FcConferenceCall /> },
+    { title: '고민상담', data: '고민상담', icon: <FcFaq /> },
+    { title: '공지사항', data: '공지사항', icon: <FcAdvertising /> },
+  ];
+
+  const handleListItemClick = (index) => {
+    setSelectedItem(index);
+    setSelectedItemData(boardListItems[index].data);
+  };
+
   return (
-    <div className='Community-container'>
-      <h1>커뮤니티</h1>
+    <div className='community-container'>
+      <div className='board-list'>
+        <ul>
+          {boardListItems.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => handleListItemClick(index)}
+              className={selectedItem === index ? 'selected-item' : ''}
+              >
+              {item.icon} {item.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='community-scroll'>
+        <div className='scroll-view'>
+          {selectedItemData && <p>{selectedItemData}</p>}
+          {/* 여기에 필요한 데이터를 표시하는 컴포넌트 추가 */}
+          <Community_Item />
+          <Community_Item />
+          <Community_Item />
+          <Community_Item />
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/AfterLogin/Home.js
+++ src/pages/AfterLogin/Home.js
@@ -1,15 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import './Home.css';
+import Mate_Item from '../../item/Mate_Item';
 
-export default function Home() {
+export const images = [
+  "/mainPhoto_1.webp",
+  "/mainPhoto_2.webp",
+  "/mainPhoto_3.webp",
+  "/mainPhoto_4.webp",
+  "/mainPhoto_5.webp",
+];
+
+const Home = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const setScrollableStyle = () => {
+    const mateRecommend = document.querySelector('.Mate-recommend');
+    if (mateRecommend) {
+      mateRecommend.style.whiteSpace = 'nowrap';
+    }
+  };
+
+  useEffect(() => {
+    setScrollableStyle();
+
+    // Set up automatic slideshow
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [currentImageIndex]);
+
   return (
     <div className='Home-container'>
-      <h1>홈페이지 로그인 후</h1>
-      <div className='Home-photoList'><h4>사진리스트</h4></div>
-      <div className='Home-slider1'><h4>요리 슬라이드</h4></div>
-      <div className='Home-slider2'><h4>원룸 슬라이드</h4></div>
-      <div className='Home-slider3'><h4>자취메이트</h4></div>
-      <div className='Home-slider3'><h4>커뮤니티</h4></div>
+      <div className='Home-photoList'>
+        <img
+          className="card-photo"
+          src={images[currentImageIndex]}
+          alt="로고"
+        />
+      </div>
+      <h4>요리 (추천 레시피 / 인기 급상승 레시피)</h4>
+      <div className='Home-Cook'>
+      </div>
+      <h4>원룸 (추천 원룸 / 원룸 검색창)</h4>
+      <div className='Home-OneRoom'>
+      </div>
+      <h4>자취메이트 (추천 메이트)</h4>
+      <div className='Home-Mate'>
+        <Mate_Item />
+        <Mate_Item />
+        <Mate_Item />
+        <Mate_Item />
+        <Mate_Item />
+      </div>
+      <h4>커뮤니티(핫 게시물)</h4>
+      <div className='Home-Community'>
+        </div>
+        <h4>생활꿀팁 (청소편) -  유튜브 연결</h4>
+      <div className='Home-Tip'>
+        </div>
     </div>
   );
-}
+};
+
+export default Home;
