be7f0a001a5001c2cd540e54f616ae891c4c6cbf
cw1662 <chaewon1662@naver.com>
2024-03-31 23:56:17 +0900

    add market category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Category.css';
 
 const Category = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null); // 선택된 카테고리 상태 초기화
+
   const categories = ["홈", "요리", "원룸", "자취메이트", "중고마켓", "커뮤니티"];
+
+  const handleCategoryClick = (index) => {
+    setSelectedCategory(index); // 클릭된 카테고리의 인덱스를 선택된 카테고리 상태로 설정
+  };
 
   return (
     <div className="category-container">
@@ -17,7 +23,8 @@ const Category = () => {
           category === "중고마켓" ? '/Market' : 
           category === "커뮤니티" ? '/Community' : 
           `/${category.toLowerCase()}`}
-          className="category-item"
+          className={`category-item ${selectedCategory === index ? 'active' : ''}`} // 선택된 카테고리에 active 클래스 추가
+          onClick={() => handleCategoryClick(index)} // 카테고리 클릭 시 선택된 카테고리 변경
         >
           {category}
         </Link>
--- src/pages/BeforeLogin/Market.js
+++ src/pages/BeforeLogin/Market.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
 import "./css/Market.css";
 import Market_Item from '../../item/Market_Item.js';
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { IoSearchOutline } from "react-icons/io5";
 
 export default function Market() {
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div className="market-container">
+        <div className="market-container"
+
+        >
             <div className="market-header">
-                <p>검색</p>
+                <div className="market-category-icon"
+                           onMouseEnter={() => setIsHovered(true)}
+                           onMouseLeave={() => setIsHovered(false)}
+                >
+                    <HiOutlineBars3 style={{fontSize: '40px'}}/>
+                </div>
+                <IoSearchOutline className='market-search-icon'/>
+                <input type="text" className="market-search-input" placeholder="상품명을 입력하세요."/>
             </div>
-            <div className="market-category">
-                <p>카테고리</p>
-            </div>
+            {isHovered && (
+                <div className="market-category"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    {/* 카테고리 목록 */}
+                    <p> 의류</p>
+                    <p> 신발</p>
+                    <p> 가전</p>
+                    <p> 가구/인테리어</p>
+                    <p> 식품</p>
+                    <p> 도서</p>
+                </div>
+            )}
             <div className="market-item">
                 <Market_Item />
                 <Market_Item />
