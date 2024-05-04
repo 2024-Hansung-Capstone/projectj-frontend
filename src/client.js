b51aa8d7c290511eef0a3078aa4999a3faa0081c
cw1662 <chaewon1662@naver.com>
2024-05-04 21:18:21 +0900

    url

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://13.209.3.204:5000/graphql:',
+  uri: 'http://13.209.72.136:5000/graphql:',
   cache: new InMemoryCache(),
 });
 
