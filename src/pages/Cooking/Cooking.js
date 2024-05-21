c984944ab781a123b41d3dc99be425bfb10d69ff
kimjunghwannn <aa2002dcks@naver.com>
2024-05-21 16:14:49 +0900

    Cooking 인기게시글 및 조회수 기능 추가

--- src/pages/Cooking/Cooking.js
+++ src/pages/Cooking/Cooking.js
@@ -67,26 +67,51 @@ const CREATE_INGREDIENT = gql`
     }
   }
 `;
+const DELETE_INGREDIENT = gql`
+  mutation DeleteIngredient($ingredientId: String!) {
+    deleteIngredient(ingredient_id: $ingredientId)
+  }
+`;
+
+const INCREASE_COOK_VIEW = gql`
+  mutation IncreaseCookView($id: String!) {
+    increaseCookView(id: $id) 
+  }
+`;
+
+const FETCH_COOKS_BY_VIEW_RANK = gql`
+  query FetchCooksByViewRank($rank: Int!) {
+    fetchCookByViewRank(rank: $rank) {
+      id
+      name
+      detail
+    }
+  }
+`;
 export default function Cooking() {
   const { loading, error, data } = useQuery(FETCH_ALL_COOKS);
+  const [increaseView] = useMutation(INCREASE_COOK_VIEW);
   const navigate = useNavigate();
   const [keyword, setKeyword] = useState('');
   const { loading: loadingSearch, error: errorSearch, data: dataSearch } = useQuery(SEARCH_COOK, {
     variables: { keyword },
     skip: !keyword
   });
-  const { loadingIngredients, errorIngredients, dataIngredients ,refetch} = useQuery(FETCH_MY_INGREDIENTS);
+  const { loading: loadingIngredients,error: errorIngredients, data: dataIngredients ,refetch} = useQuery(FETCH_MY_INGREDIENTS);
+  const { loading:toploading, error:toperror, data :topdata} = useQuery(FETCH_COOKS_BY_VIEW_RANK, {
+    variables: {  rank:1.0 },
+  });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loggedInUserName, setLoggedInUserName] = useState('');
   const [ingredientName, setIngredient] = useState('');
   const [volume, setQuantity] = useState('');
   const [volume_unit, setUnit] = useState('');
   const { Search } = Input;
-  const [createIngredient, { data:dataCreated, loading:loadingCreated, error :errorCreated}] = useMutation(CREATE_INGREDIENT);
+  const [createIngredient] = useMutation(CREATE_INGREDIENT);
+  const [deleteIngredient] = useMutation(DELETE_INGREDIENT);
   useEffect(() => { // 토큰
     const token = localStorage.getItem('token');
     setIsLoggedIn(!!token);
-    
     const loggedInUser = localStorage.getItem('loggedInUserName'); // 로그인 유저
     if (loggedInUser) {
       setLoggedInUserName(loggedInUser);
@@ -110,8 +135,16 @@ export default function Cooking() {
     navigate("/CookingAI");
     
   };
-  // 레시피 상세 페이지로 이동
+  // Cook 상세 페이지로 이동
   const handleItemClick = (cook) => {
+    increaseView({ variables: { id: cook.id }}).then(response => {
+      console.log('조회수가 증가되었습니다.', response.data);
+    })
+    .catch(err => {
+      console.error('조회수 증가 에러:', err);
+      console.log(JSON.stringify(err, null, 2))
+
+    });
     navigate("/CookingDetails", { state: { cook } });
   };
 
@@ -130,12 +163,11 @@ export default function Cooking() {
           },
         });
         alert('식재료가 성공적으로 추가되었습니다.');
-        await refetch()
+        await refetch();
       } catch (error) {
         console.error('식재료 추가 중 오류 발생:', error);
         alert('식재료 추가 중 오류가 발생했습니다.');
-        console.log(JSON.stringify(error, null, 2))
-        console.log(JSON.stringify(errorIngredients, null, 2))
+        
       }
       setIngredient('');
       setQuantity('');
@@ -159,8 +191,13 @@ export default function Cooking() {
     return (
       <div className="cooking-item">
         <div className='header'>
-                {/* <h1>BEST 레시피</h1> */}
-              </div>
+        {
+          (topdata && topdata.fetchCookByViewRank ? (
+          topdata.fetchCookByViewRank.map((cook) => (
+          <p key={cook.id}>인기 요리 이름: {cook.name}</p>))) : 
+        (<p>데이터를 가져오는 중 에러가 발생했습니다.</p>))}
+
+        </div>
         <div className='cooking-items-container'>
           <p>전체 레시피</p>
           <div className='cooking-dishes-grid'>
@@ -172,13 +209,30 @@ export default function Cooking() {
       </div>
     );
   };
+  const handleDeleteIngredient = async (ingredientId) => {
+    try {
+      // 삭제 요청 보내기
+      await deleteIngredient({
+        variables: {
+          ingredientId: ingredientId,
+        },
+      });
+      // 삭제 성공 시 재료 다시 불러오기
+      await refetch();
+      alert('식재료가 성공적으로 삭제되었습니다.');
+    } catch (error) {
+      console.log(JSON.stringify(error, null, 2))
 
+      console.error('식재료 삭제 중 오류 발생:', error);
+      alert('식재료 삭제 중 오류가 발생했습니다.');
+    }
+  };
 
 
 
   return (
     <div className='cooking-container'>
-      <div className='cook-ai-container'>
+      <div className='cook-ai-container'>  
         <h2>AI 레시피</h2>
 
         {/* AI 검색 */}
@@ -204,12 +258,12 @@ export default function Cooking() {
               {dataIngredients?.fetchMyIngredients.length === 0 ? (
                 <p className="nodata">재료가 없습니다.</p>
                 ) : (
-                  dataIngredients?.fetchMyIngredients.map((ing, index) => (
-                <div key={index} className="ingredient-item">
+                  dataIngredients?.fetchMyIngredients.map((ing) => (
+                <div className="ingredient-item" key={ing.id}>
                   <span style={{ margin: '10px 10px 0 10px' }}>{ing.name}</span>
                   <span>{ing.volume}</span>
                   <span>{ing.volume_unit}</span>
-                  <span>hidddddddddddddddddddddddddddd</span>
+                  <button onClick={() => handleDeleteIngredient(ing.id)}>삭제</button>
                 </div>)))}
               {/* AI 검색 결과 페이지로 이동 */}
               <button onClick={handleAISearch}>➔</button>
